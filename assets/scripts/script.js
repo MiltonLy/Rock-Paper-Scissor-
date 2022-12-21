1cb500795711d73b671fb9ad8c0ace2c60e3c9f2
Brandon Haskell <bdh747flyna1@hotmail.com>
2022-12-21 15:36:55 -0800

    added final code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,25 +3,55 @@ var lossCounter = 0;
 var tieCounter = 0;
 var roundCounter = 0;
 var userInput = "";
+var pcHand = "";
 
 const validValues = ["R", "P", "S"];
 
-alert("Welcome to Rack Paper Scissors!");
-
 // If user does NOT want to play, end program
-if(!confirm("Would you like to play!")){
-    return 0;
+if(!confirm("Welcome to Rack Paper Scissors!\n\nWould you like to play?")){
+    // return;
 }
 
-roundCounter += 1;
-userInput = prompt("Round: " + roundCounter + "\n\nEnter P, R, S").toUpperCase;
+do{
+    roundCounter += 1;
+    userInput = promptUser();
+    pcHand = getPcHand();
+    scoreRound(userInput, pcHand);
+} while(playAgain());
 
-function validInput(userInput){
+function scoreRound(userInput, pcHand){
+    
+    if( userInput === pcHand){
+        tieCounter += 1;
+    }
+    else if( (userInput === "R" && pcHand === "S") ||
+        (userInput === "P" && pcHand === "R") ||
+        (userInput === "S" && pcHand === "P")){
+            winCounter +=1;
+    } else {
+        lossCounter += 1;
+    }
+}
+
+function promptUser(){
+    userInput = prompt("Round: " + roundCounter + "\n\nWins: " + winCounter + "\nLosses: " + lossCounter + "\nTies: " + tieCounter + "\n\nEnter P, R, S");
+    userInput = userInput.toUpperCase();
+    if( !isValidInput(userInput)){
+        promptUser();
+    }
+    return userInput;
+}
+
+function playAgain(){
+    return confirm("Round: " + roundCounter + "\n\nWins: " + winCounter + "\nLosses: " + lossCounter + "\nTies: " + tieCounter + "\n\nPlay again?");
+}
+
+function isValidInput(userInput){
     var test = false;
 
     // if user entered more than one char
     // we got bad input
-    if( userInput.length > 1 ){
+    if( userInput.length > 1 || userInput === null){
         return test;
     }
 
@@ -32,4 +62,11 @@ function validInput(userInput){
     }
 
     return test;
+}
+
+function getPcHand(){
+    var hand = "";
+
+    hand = validValues[(Math.floor(Math.random() * validValues.length))];
+    return hand;
 }
